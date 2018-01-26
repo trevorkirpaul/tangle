c44f0d254fbbe4a31dca426891f233c7c2e40f45
trevorKirpaul <tkirpaul@gmail.com>
2018-01-26 02:26:52 -0500

    pulled posts into new component and added styling

--- src/components/posts/PostsList.js
+++ src/components/posts/PostsList.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import Progress from 'material-ui/CircularProgress';
-import { Link } from 'react-router-dom';
+import PostItem from './PostItem';
+import styled from 'styled-components';
+
+const List = styled.ul`
+  list-style: none;
+  padding: 0;
+  margin: 10px;
+`;
+
 export default ({ posts, loading, error }) => {
   if (posts) {
     return (
-      <ul>
-        {posts.map(post => (
-          <li key={post._id}>
-            <Link to={`/post/${post._id}`}>{post.title}</Link> by{' '}
-            {post.author.email}
-          </li>
-        ))}
-      </ul>
+      <List>{posts.map(post => <PostItem key={post._id} {...post} />)}</List>
     );
   } else if (loading) {
     return (
